55a98bf19b725ad3ad1a3b0f53ab9587d4d0641f
Harris Kwong <harriskwong120898@gmail.com>
2023-05-07 11:55:12 -0400

    Handle player movements.

--- game.py
+++ game.py
@@ -1,5 +1,6 @@
 import pygame
 from gameObject import GameObject
+from player import Player
 
 
 class Game:
@@ -18,7 +19,7 @@ class Game:
 
         self.background = GameObject(0,0,self.width,self.height,'asset/background.png')
         self.treasure = GameObject(375,50,50,50,'asset/treasure.png')
-     
+        self.player = Player(375,700,50,50,'asset/player.png',10)
 
 
     def draw_objects(self):
@@ -27,20 +28,37 @@ class Game:
         #set image where on the screen 
         self.game_window.blit(self.background.image,(self.background.x,self.background.y))
         self.game_window.blit(self.treasure.image,(self.treasure.x,self.treasure.y))
+        self.game_window.blit(self.player.image,(self.player.x,self.player.y))
         pygame.display.update()
 
 
 
 
     def run_game_loop(self):
+
+        player_direction = 0
+
         while True:
             #handle events
             events = pygame.event.get()
             for event in events:
                 if event.type == pygame.QUIT:
                     return
+                elif event.type == pygame.KEYDOWN:
+                    #move player up
+                    if event.key == pygame.K_UP:
+                        player_direction = -1
+                    #move player down    
+                    elif event.key == pygame.K_DOWN:
+                        player_direction = 1
+                elif event.type == pygame.KEYUP:
+                    if event.key == pygame.K_UP or event.key == pygame.K_DOWN:
+                        player_direction = 0
 
+                        
             #Execute logic
+            self.player.move(player_direction)
+
             #Update display
             self.draw_objects()
 
