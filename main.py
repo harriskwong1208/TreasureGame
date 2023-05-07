be9b873f16e8281f95689ecf5a6ba183a302134d
Harris Kwong <harriskwong120898@gmail.com>
2023-05-07 10:54:18 -0400

    Added assets . Background to window.

--- main.py
+++ main.py
@@ -13,6 +13,11 @@ white_color = (255,255,255)
 
 clock = pygame.time.Clock() 
 
+#load background image
+background_image = pygame.image.load('asset/background.png')
+#scale background image
+background = pygame.transform.scale(background_image,(width,height))
+
 def run_game_loop():
     while True:
         #handle events
@@ -24,6 +29,7 @@ def run_game_loop():
         #Execute logic
         #Update display
         game_window.fill(white_color)
+        game_window.blit(background,(0,0))
         pygame.display.update()
 
         clock.tick(60)
